2684c988a6426ac2968bfb4fef50fdc419c8c29c
HYSKAWUNNAFLI <ductranphamminh4924@gmail.com>
2024-12-05 23:34:29 +0700

    Add about in home landing page

--- src/routes/defaultRouters.js
+++ src/routes/defaultRouters.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const defaultController = require("../controllers/defaultController");
 
 router.get("/", defaultController.show);
+router.get("/about", defaultController.show_about);
 
 module.exports = router;
