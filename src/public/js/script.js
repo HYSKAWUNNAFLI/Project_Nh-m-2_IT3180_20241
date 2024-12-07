8fa555479aa2cd803be4f0dd8646efd79f5d1dcf
ScratwoS <minhquangcva4@gmail.com>
2024-12-07 23:21:10 +0700

    quang-fixed

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,3 +1,4 @@
+/*-------------------------Menu-hover--------------------------*/
 document.addEventListener("DOMContentLoaded", function () {
     const allNavItems = document.querySelectorAll("#navbar li"); // Tất cả các mục trong navbar
     const overlay = document.querySelector(".fullscreen-overlay"); // Lớp phủ
@@ -60,3 +61,26 @@ document.addEventListener("DOMContentLoaded", function () {
         productItem.addEventListener("mouseleave", hideDropdown);
     }
 });
+/*---------------------------------------------------*/
+
+/*---------------------Account-page------------------*/
+// Lấy các phần tử cần thiết
+const loginForm = document.getElementById('LoginForm');
+const regForm = document.getElementById('RegForm');
+const indicator = document.getElementById('Indicator');
+
+// Hàm để chuyển sang form Login
+function switchToLogin() {
+    loginForm.style.transform = 'translateX(0)';
+    regForm.style.transform = 'translateX(300px)';
+    indicator.style.transform = 'translateX(0)'; // Gạch chân di chuyển về Login
+}
+
+// Hàm để chuyển sang form Register
+function switchToRegister() {
+    loginForm.style.transform = 'translateX(-300px)';
+    regForm.style.transform = 'translateX(0)';
+    indicator.style.transform = 'translateX(100%)'; // Gạch chân di chuyển về Register
+}
+
+/*----------------------------------------*/
