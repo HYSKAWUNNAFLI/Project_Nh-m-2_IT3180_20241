19eba605b76054186cdc8416d405c8459daa8e70
HYSKAWUNNAFLI <ductranphamminh4924@gmail.com>
2024-12-12 17:01:53 +0700

    Finish authenticator for checkout

--- src/routes/cartRouters.js
+++ src/routes/cartRouters.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cartController");
 const db = require('../models/db')
+const { authenticator_for_checkout } = require("../middleware/authMiddleware"); // Đảm bảo import đúng middleware
 
 router.get("/", cartController.show);
 router.post("/update", cartController.updateQuantity);
 router.post("/delete", cartController.deleteProduct);
 
-router.get('/checkout', cartController.showCheckout);
+router.get("/checkout", authenticator_for_checkout, cartController.showCheckout);
 // Định nghĩa route xử lý POST yêu cầu Cash On Delivery (COD)
 // Định nghĩa route GET cho /cart/checkout/cod
 router.get('/checkout/cod', cartController.showThankYouPage);
